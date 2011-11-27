677acbb948e33360d70c622361193ccdd25aab84
Atte Hinkka <atte.hinkka@iki.fi>
2011-11-27 15:37:46 +0200

    Code cleanup, refactoring

--- Parser.py
+++ Parser.py
@@ -1,53 +1,64 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import sys
 import os
 import sys
 import re
-import logging as logger
-from datetime import datetime
+import logging
 import gzip
+
+from datetime import datetime
 from bz2 import BZ2File
+
+logger = logging.getLogger("Parser")
 
 # 123.123.123.1 - - [15/Nov/2009:06:50:06 +0200] "GET / HTTP/1.1" 404 995 "http://www.kapsi.fi/" "USER AGENT"
 # XXXXX tämä matchataan XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX --------ei väliä-------------------
-PATTERN = re.compile('^(?P<ip>[^ ]+) [^ ]+ [^ ]+ \[(?P<date>[^\]]+)\] ".+(?<!\\\)" (?P<status>\d+) (?P<size>\d+|-)')
+LOG_ENTRY_PATTERN = re.compile('^(?P<ip>[^ ]+) [^ ]+ [^ ]+ \[(?P<date>[^\]]+)\] ".+(?<!\\\)" (?P<status>\d+) (?P<size>\d+|-)')
+
+MONTH_NUMBERS = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05',
+          'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08', 'Sep': '09',
+          'Oct': '10', 'Nov': '11', 'Dec': '12'}
+
 
 def parse_line(line):
-    result = re.match(PATTERN, line)
+    result = re.match(LOG_ENTRY_PATTERN, line)
     ip = result.group('ip')
     time = result.group('date')
     status = result.group('status')
     size = result.group('size')
     return { 'ip': ip, 'date': time[0:11], 'status': status, 'size': size}
 
-MONTHS = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05',
-          'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08', 'Sep': '09',
-          'Oct': '10', 'Nov': '11', 'Dec': '12'}
+
 def date_convert(date):
     try:
 	logger.debug("date_convert: " + date)
         day, month, year = date.split("/")
-        return "%s-%s-%s" % (year, MONTHS[month], day)
+        return "%s-%s-%s" % (year, MONTH_NUMBERS[month], day)
     except:
         return "0000-00-00"
 
-class Parser():
-    '''Class for parsing Apache access log
-    Parses the access log line by line and keeps track of the size and
-    number of all requests, for each day.'''
-    def __init__(self):
-        self.d = {}
+
+class Parser(object):
+    '''
+    Class for parsing Apache access log
     
+    Parses files line by line and keeps track of the size and number of all
+    requests for each day.
+    '''
+    def __init__(self):
+        self.date = {}
+
     def parse(self, line):
         parsed = parse_line(line)
         size = parsed['size']
         date = date_convert(parsed['date'])
-        if not self.d.get(date):
-            self.d[date] = [0, 0] # bandwidth, hits
+        if not self.date.get(date):
+            self.date[date] = [0, 0] # bandwidth, hits
         if not size == '-':
-            self.d[date][0] += int(size)
-            self.d[date][1] += 1
+            self.date[date][0] += int(size)
+            self.date[date][1] += 1
 
     def parse_file(self, filename):
         f = None
@@ -72,25 +83,30 @@ class Parser():
 
     def valid_data(self):
         '''Discard values for the first and last date'''
-        dates = sorted(self.d.keys(), key=lambda x: x[0])
+        dates = sorted(self.date.keys(), key=lambda x: x[0])
         try:
             del dates[0]
             del dates[-1]
         except:
             return {}
-        d = {}
-        for a, b in dates:
-            d[a] = self.d[b]
-        return d
-    
+
+        ret = {}
+        for date in dates:
+            ret[date] = self.date[date]
+        return ret
+
+
 def main():
     filelist = sys.argv[1:]
-    print filelist
+    print >> sys.stderr, "# Parsing files: %s" % ', '.join(filelist)
+
     p = Parser()
     for f in filelist:
         p.parse_file(f)
-    for (key, value) in sorted(p.d.items()):
+
+    for key, value in sorted(p.date.items()):
         print "%s %8.0f MiB %8d reqs" % (key, value[0]/1024/1024, value[1])
-    
+
+
 if __name__ == '__main__':
     main()
--- parselogs.py
+++ parselogs.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
 import sys
 import os
 import os.path
-from Parser import Parser
-from model import Usage
-from sqlobject import connectionForURI, sqlhub
-from socket import gethostname
 import datetime
 import logging
+
+from socket import gethostname
+
+from sqlobject import connectionForURI, sqlhub
+
+from Parser import Parser
+from model import Usage
+
 logging.basicConfig(level=logging.DEBUG,
     format='%(asctime)s %(name)s %(levelname)s %(message)s')
 logger = logging.getLogger('parselogs')
 
-LOGDIR = '/var/log/apache2/vhosts'
-SERVER = gethostname()
-DB = 'sqlite:' + os.path.join(os.getcwd(), 'www.sqlite')
-
-def main():
-    sqlhub.processConnection = connectionForURI(DB)
-    for domain in os.listdir(LOGDIR):
+def main(log_dir, server_name, database_spec):
+    sqlhub.processConnection = connectionForURI(database_spec)
+    for domain in os.listdir(log_dir):
         logger.info("Parsing logs for domain " + domain)
-        logfile = os.path.join(LOGDIR, domain, 'access.log')
+        logfile = os.path.join(log_dir, domain, 'access.log')
         p = Parser()
         p.parse_file(logfile)
         p.parse_file(logfile+'.1')
@@ -29,13 +30,29 @@ def main():
             bytes, hits = d
             date_d = datetime.date(*[int(x) for x in date.split("-")])
             try:
-                u = Usage.selectBy(date=date_d, domain=domain, server=SERVER)[0]
-		continue
+                u = Usage.selectBy(date=date_d, domain=domain, server=server_name)[0]
+                continue
             except:
-                Usage(server=SERVER, domain=domain, date=date,
-                    bytes=bytes, hits=hits)
+                Usage(server=server_name, domain=domain, date=date,
+                      bytes=bytes, hits=hits)
                 logger.debug("Added %s for domain %s" % (date, domain))
 
+
 if __name__ == '__main__':
-    main()
+    from optparse import OptionParser
+
+    parser = OptionParser()
+
+    parser.add_option("-l", "--log-dir", dest="log_dir",
+                      help="read logs from DIRECTORY with vhost log dirs", metavar="DIRECTORY",
+                      default='/var/log/apache2/vhosts')
+    parser.add_option("-s", "--server-name", dest="server_name",
+                      help="override the canonical hostname", metavar="SERVER-NAME",
+                      default=gethostname())
+    parser.add_option("-d", "--database-spec", dest="db_spec",
+                      help="database to connect to", metavar="DB-SPEC",
+                      default='sqlite:' + os.path.join(os.getcwd(), 'www.sqlite'))
+
+    opts, args = parser.parse_args()
+    main(opts.log_dir, opts.server_name, opts.db_spec)
 
